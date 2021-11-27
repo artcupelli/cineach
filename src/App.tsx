c804df6954d7635cec20cdf58aa8a0ecbd833fc0
Artur Cupelli <artur.cupelli@usp.br>
2021-11-27 10:18:56 -0300

    Configura constantes de cores

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Badge } from 'evergreen-ui';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Badge color="green" >Teste</Badge>
     </div>
   );
 }
